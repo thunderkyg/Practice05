d33bb914b8a302976827fae8521e6bc95f4a441d
thunderkyg <thunderkyg@gmail.com>
2021-06-07 15:01:02 +0900

    Practice05 (2021/06/07)

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,72 +1,57 @@
 package com.javaex.ex10;
 
 public class Book {
+    private int bookNo;
+    private String title;
+    private String author;
+    private int stateCode;
 
-	// 필드
-	int bookNo;
-	String title;
-	String author;
-	int stateCode;
+    public Book(){}
+    
+    public Book(int bookNo, String title, String author) {
+        this.bookNo = bookNo;
+        this.author = author;
+        this.title = title;
+        this.stateCode = 1;
+    }
 
-	// 생성자
-	public Book() {
+    public void rent() {
+        stateCode = 0;
+        System.out.println(title + "이(가) 대여 됐습니다.");
+    }
 
-	}
+    public void print() {
+    	String rentState;
+    	if( stateCode == 1 ) {
+    		rentState = "재고있음";
+    	}else {
+    		rentState = "대여중";
+    	}
+        System.out.println(bookNo + " 책 제목:" + title + ", 작가:" + author + ", 대여 유무:" + rentState);
+    }
 
-	public Book(int bookNo, String title, String author) {
-		this.bookNo = bookNo;
-		this.title = title;
-		this.author = author;
-	}
-	
-	public Book(int bookNo, String title, String author, int stateCode) {
-		super();
-		this.bookNo = bookNo;
-		this.title = title;
-		this.author = author;
-		this.stateCode = stateCode;
-	}
-	
-	//메소드 - G/S
-	
-	public int getBookNo() {
-		return bookNo;
-	}
-	
-	public String getTitle() {
-		return title;
-	}
-	
-	public String getAuthor() {
-		return author;
-	}
-	
-	public int getStateCode() {
-		return stateCode;
-	}
-	
-	public void setBookNo(int bookNo) {
-		this.bookNo = bookNo;
-	}
-	
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	
-	public void setAuthor(String author) {
-		this.author = author;
-	}
-	
-	public void setStateCode(int stateCode) {
-		this.stateCode = stateCode;
-	}
+    public int getBookNo() {
+        return bookNo;
+    }
 
+    public void setBookNo(int bookNo) {
+        this.bookNo = bookNo;
+    }
 
-	//메소드 - 일반
-	public void rent() {
-	}
-	
-	public void print() {
-		
-	}
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 }
